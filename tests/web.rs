7cfc4f67eb4950ef27268ab7e4bf061e06450065
Gyanendra Shukla <srirajshukla@gmail.com>
2021-06-11 22:46:26 +0530

    moved colors tests to colors file

--- tests/web.rs
+++ tests/web.rs
@@ -7,29 +7,25 @@ use wasm_bindgen_test::*;
 
 extern crate dp_vis;
 use dp_vis::*;
+use dp_vis::Pendulum;
 // wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 // #[cfg(test)]
 fn dp (){
     let pi = std::f64::consts::PI;
-    let p = Pendulam::new(10.0, 10.0, 0.0, pi/2.0, 50.0, 50.0, 0.01);
+    let p = Pendulum::new(10.0, 10.0, 0.0, pi/2.0, 50.0, 50.0, 0.01);
     assert_eq!(p.x1(), 0.0);
 }
 
 #[wasm_bindgen_test]
 fn dnpvec(){
     let pi = std::f64::consts::PI;
-    let p = Pendulam::new(10.0, 10.0, 0.0, pi/2.0, 50.0, 50.0, 0.01);
-    let mut pv = PendulamVector::new();
+    let p = Pendulum::new(10.0, 10.0, 0.0, pi/2.0, 50.0, 50.0, 0.01);
+    let mut pv = PendulumVector::new();
 
     pv.add(p);
 
     assert_eq!(pv.item_x1(0), 0.0);
 }
 
-#[wasm_bindgen_test]
-fn testcolor(){
-    let red = Colors::new(248, 0, 0, 0.8);
-    assert_eq!(red.rgba(), String::from("rgba(248, 0, 0, 0.8)"));
-}
